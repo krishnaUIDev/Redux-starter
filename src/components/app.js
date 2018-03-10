ab7d03ab9aeb6af0a3813fdf609179f1b3fb28ea
krishna <krishna.kondoju007@gmail.com>
2018-03-10 13:36:02 -0500

    added

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-
+import Home from './home/index'
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Home />
+      </div>
     );
   }
 }
